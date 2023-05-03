90aed2f5605bda2eb632c12565413a91dc3b5fdf
levosay <ryuuzaki44l@gmail.com>
2023-05-03 23:06:54 +0300

    refactor prepareQuestionsData

--- src/modules/TestModule/TestModule.tsx
+++ src/modules/TestModule/TestModule.tsx
@@ -10,12 +10,8 @@ import {
   QuestionsSequence,
   QuestionsText
 } from 'components'
-import {
-  IHookFormValues,
-  IQuestionData,
-  IQuestionsCheckProps,
-  IQuestionsTextProps
-} from '@/src/types/forms'
+import { IHookFormValues, } from 'types/forms'
+import { prepareQuestionsData } from 'utils/helpers/preparaQuestionsData'
 
 export const TestModule: FunctionComponent<
   ITestModuleProps
@@ -31,74 +27,9 @@ export const TestModule: FunctionComponent<
     }
   })
 
-  const regQuestionsId = new RegExp(/_[a-z|0-9]*_/)
-  const regAnswerId = new RegExp(/_[0-9]$/)
-  const regTypeCheck = new RegExp(/^questionCheck/)
-  const regTypeText = new RegExp(/^questionText/)
-
-  const getQuestionsId = (string: string) => {
-    return string.match(regQuestionsId)?.shift()?.replace(/^_/g, '')
-      .replace(/_$/g, '') || ''
-  }
-
-  const getAnswerId = (string: string) => {
-    return string.match(regAnswerId)?.shift()?.replace(/^_/g, '') || ''
-  }
-
-  const getQuestionsType = (string: string) => {
-    return string.match(regTypeCheck)
-      ?.shift() || string.match(regTypeText)?.shift() || ''
-  }
-
-  const matchQuestionsTypeCheck = (string: string) => regTypeCheck.test(string)
-
-  const matchQuestionsTypeText = (string: string) => regTypeText.test(string)
-
-  const createQuestionsCheck = ({ obj, key, value }: IQuestionsCheckProps) => {
-    obj[getQuestionsId(key)] = {}
-    obj[getQuestionsId(key)].id = getQuestionsId(key)
-    obj[getQuestionsId(key)].type = getQuestionsType(key)
-    obj[getQuestionsId(key)].answers = [{
-      id: getAnswerId(key), value: value
-    }]
-  }
-
-  const updateQuestionsCheck = ({ obj, key, value }: IQuestionsCheckProps) => {
-    obj[getQuestionsId(key)].answers = [...obj[getQuestionsId(key)].answers, {
-      id: getAnswerId(key), value: value
-    }]
-  }
-
-  const createQuestionsText = ({ obj, key, value }: IQuestionsTextProps) => {
-    obj[getQuestionsId(key)] = {}
-    obj[getQuestionsId(key)].id = getQuestionsId(key)
-    obj[getQuestionsId(key)].type = getQuestionsType(key)
-    obj[getQuestionsId(key)].answers = [{
-      id: getQuestionsId(key),
-      value: value
-    }]
-  }
-
   const submit = (data: IHookFormValues) => {
-    const obj: IQuestionData = {}
-
-    for (const key in data) {
-      const validKey = key as keyof IHookFormValues
-      if (matchQuestionsTypeCheck(key)) {
-
-        if (obj[getQuestionsId(key)]) {
-          updateQuestionsCheck({ obj, key, value: data[validKey] as boolean })
-        } else {
-          createQuestionsCheck({ obj, key, value: data[validKey] as boolean })
-        }
-      }
-
-      if (matchQuestionsTypeText(key)) {
-        createQuestionsText({ obj, key, value: data[validKey] as string })
-      }
-    }
-
-    console.log('obj_______ ', obj)
+    const body = prepareQuestionsData(data)
+    console.log('obj_______ ', body)
   }
 
   return (
---
+++ src/utils/helpers/preparaQuestionsData.ts
@@ -0,0 +1,76 @@
+import {
+  IHookFormValues,
+  IQuestionData,
+  IQuestionsCheckProps,
+  IQuestionsTextProps
+} from 'types/forms'
+
+const regQuestionsId = new RegExp(/_[a-z|0-9]*_/)
+const regAnswerId = new RegExp(/_[0-9]$/)
+const regTypeCheck = new RegExp(/^questionCheck/)
+const regTypeText = new RegExp(/^questionText/)
+
+const matchQuestionsTypeCheck = (string: string) => regTypeCheck.test(string)
+const matchQuestionsTypeText = (string: string) => regTypeText.test(string)
+
+const getQuestionsId = (string: string) => {
+  return string.match(regQuestionsId)?.shift()?.replace(/^_/g, '')
+    .replace(/_$/g, '') || ''
+}
+
+const getAnswerId = (string: string) => {
+  return string.match(regAnswerId)?.shift()?.replace(/^_/g, '') || ''
+}
+
+const getQuestionsType = (string: string) => {
+  return string.match(regTypeCheck)
+    ?.shift() || string.match(regTypeText)?.shift() || ''
+}
+
+const createQuestionsCheck = ({ obj, key, value }: IQuestionsCheckProps) => {
+  obj[getQuestionsId(key)] = {}
+  obj[getQuestionsId(key)].id = getQuestionsId(key)
+  obj[getQuestionsId(key)].type = getQuestionsType(key)
+  obj[getQuestionsId(key)].answers = [{
+    id: getAnswerId(key), value: value
+  }]
+}
+
+const updateQuestionsCheck = ({ obj, key, value }: IQuestionsCheckProps) => {
+  obj[getQuestionsId(key)].answers = [...obj[getQuestionsId(key)].answers, {
+    id: getAnswerId(key), value: value
+  }]
+}
+
+const createQuestionsText = ({ obj, key, value }: IQuestionsTextProps) => {
+  obj[getQuestionsId(key)] = {}
+  obj[getQuestionsId(key)].id = getQuestionsId(key)
+  obj[getQuestionsId(key)].type = getQuestionsType(key)
+  obj[getQuestionsId(key)].answers = [{
+    id: getQuestionsId(key),
+    value: value
+  }]
+}
+
+export const prepareQuestionsData = (data: IHookFormValues) => {
+  const obj: IQuestionData = {}
+
+  for (const key in data) {
+    const validKey = key as keyof IHookFormValues
+
+    if (matchQuestionsTypeCheck(key)) {
+      if (obj[getQuestionsId(key)]) {
+        updateQuestionsCheck({ obj, key, value: data[validKey] as boolean })
+      } else {
+        createQuestionsCheck({ obj, key, value: data[validKey] as boolean })
+      }
+    }
+
+    if (matchQuestionsTypeText(key)) {
+      createQuestionsText({ obj, key, value: data[validKey] as string })
+    }
+  }
+
+  return obj
+}
+
