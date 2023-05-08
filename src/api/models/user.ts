31fc6b68dc6a179469c8a4de2d09b00d52754f09
levosay <ryuuzaki44l@gmail.com>
2023-05-08 21:39:39 +0300

    feat: exams interface for user

--- src/api/models/user.ts
+++ src/api/models/user.ts
@@ -1,6 +1,22 @@
+interface IReviewItem {
+  _id: string
+  title: string
+  userAnswer: string
+  passAnswer: string
+  pass: boolean
+}
+
+interface IExamItem {
+  _id: string
+  review: IReviewItem
+  date: string
+  points: number
+}
+
 export interface IUser {
   roles: string
   username: string
   __v: number
   _id: string
+  exams: IExamItem[]
 }
