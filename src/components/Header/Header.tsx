d0fcfd412fd90cd7a9e4600f9cfc97c67a8acc20
levosay <ryuuzaki44l@gmail.com>
2023-05-28 18:43:27 +0300

    test refetchInterval

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,7 @@ export const Header: FunctionComponent<IHeaderProps> = (): JSX.Element => {
   const { data, isLoading } = useQuery<IUser>({
     queryKey: ['user'],
     queryFn: getMe,
-    refetchInterval: 0
+    refetchInterval: () => data ? 0 : 2000
   })
 
   useEffect(() => {
